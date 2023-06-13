627b395932f5bbb4899f714f06a815831eda048e
Yarli Rabelo <yrish@Yarlis-MacBook-Pro.local>
2023-06-13 22:15:44 +0100

    READ.ME updated + create index.html + sketch.js + included p5.lib

---
+++ README.md
@@ -0,0 +1,4 @@
+# DrawingApp
+This is a drawing app, build with p5.js a JavaScript Library that allows us to build cool artistic stuff. 
+
+Here we are going to explore functionalities from p5.
---
+++ sketch.js
@@ -0,0 +1,18 @@
+//global variables that will store the toolbox colour palette 
+var toolbox = null;
+var colourP = null;
+var helpers = null;
+
+function setup() {
+    //create canvas to fill the content div from index.html
+    canvasContainer = select('#content');
+    var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+    c.parent('content');
+
+
+}
+
+//draw stuff into canvas
+function draw(){
+
+}
